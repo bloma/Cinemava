d14dc9c6edad788cd760fcb71c043d65e9d89808
aphiwe <blomaphiwe@gmail.com>
2025-03-27 13:24:02 +0200

    added properties to the models

--- Cinemava.Core/Models/User.cs
+++ Cinemava.Core/Models/User.cs
@@ -3,5 +3,19 @@ namespace Cinemava.Core.Models
 {
     public class User
     {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public User() { }
+        public User(Guid id, string? name, string? surname, string? email, string? password)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            Email = email;
+            Password = password;
+        }
     }
 }
